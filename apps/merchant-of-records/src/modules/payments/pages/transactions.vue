f5ac76d981213fa2bbf95b621b2082af11adc1ff
Aishat Akinyemi <aishat@vesicash.com>
2025-10-10 12:05:54 +0100

    format export data

--- apps/merchant-of-records/src/modules/payments/pages/transactions.vue
+++ apps/merchant-of-records/src/modules/payments/pages/transactions.vue
@@ -1,69 +1,69 @@
 <template>
   <PageContentWrapper>
-<template #pageOptions>
-  <div
-    class="flex  sm:flex-wrap sm:flex-row-reverse  items-center  gap-4 md:gap-6  mb-6"
-    v-if="tableBody.length > 0 && !isLoading"
-  >
-
- <div class = "flex justify-between items-center gap-4 w-full">
-  <div class="relative w-48 filter-select border rounded-md bg-grey-50/80 cursor-pointer  text-sm font-semibold text-teal-800  ">
-        <select
-          v-model="selectedMethod"
-          class=" appearance-none w-full p-4 focus:outline-none bg-transparent"
-        >
-          <option value="">Payment Method</option>
-          <option
-            v-for="(method, index) in paymentMethods"
-            :value="method"
-            :key="index"
-          >
-            {{ method }}
-          </option>
-        </select>
-        <div
-          class="absolute text-[16px] text-teal-800 -translate-y-1/2 pointer-events-none icon icon-caret-down right-4 top-1/2"
-        ></div>
-      </div>
-<div class="relative w-48 filter-select border rounded-md bg-grey-50/80 cursor-pointer text-sm font-semibold text-teal-800  ">
-        <select
-          v-model="selectedStatus"
-          class=" appearance-none w-full p-4 bg-transparent focus:outline-none"
-        >
-          <option value="">Status</option>
-          <option
-            v-for="(status, index) in statusOptions"
-            :value="status.toLowerCase()"
-            :key="index"
-          >
-            {{ status }}
-          </option>
-        </select>
-        <div
-          class="absolute text-[16px] text-teal-800 -translate-y-1/2 pointer-events-none icon icon-caret-down right-4 top-1/2"
-        ></div>
-      </div>
-
-     </div>
-    
-    <div class="flex  items-center w-full  gap-4 ">
-    
-        <DatePicker
-        
-          filterSize="lg"
-          :activePeriod="activePeriod"
-          @onFilterSelected="processFilterSelection"
-        />
-        <button
-        @click="exportToExcel"
-        class="export-btn whitespace-nowrap w-full   p-4 border rounded-md font-semibold text-sm text-teal-800 hover:bg-teal-50 transition-all duration-200"
+    <template #pageOptions>
+      <div
+        class="flex sm:flex-wrap sm:flex-row-reverse items-center gap-4 mb-6"
+        v-if="tableBody.length > 0 && !isLoading"
       >
-        Export
-      </button>
-    </div>
-  </div>
-</template>
+        <div class="flex justify-between items-center gap-4 w-full">
+          <div
+            class="relative w-48 filter-select border rounded-md bg-grey-50/80 cursor-pointer text-sm font-semibold text-teal-800"
+          >
+            <select
+              v-model="selectedMethod"
+              class="appearance-none w-full p-4 focus:outline-none bg-transparent"
+            >
+              <option value="">Payment Method</option>
+              <option
+                v-for="(method, index) in paymentMethods"
+                :value="method"
+                :key="index"
+              >
+                {{ method }}
+              </option>
+            </select>
+            <div
+              class="absolute text-[16px] text-teal-800 -translate-y-1/2 pointer-events-none icon icon-caret-down right-4 top-1/2"
+            ></div>
+          </div>
 
+          <div
+            class="relative w-48 filter-select border rounded-md bg-grey-50/80 cursor-pointer text-sm font-semibold text-teal-800"
+          >
+            <select
+              v-model="selectedStatus"
+              class="appearance-none w-full p-4 bg-transparent focus:outline-none"
+            >
+              <option value="">Status</option>
+              <option
+                v-for="(status, index) in statusOptions"
+                :value="status.toLowerCase()"
+                :key="index"
+              >
+                {{ status }}
+              </option>
+            </select>
+            <div
+              class="absolute text-[16px] text-teal-800 -translate-y-1/2 pointer-events-none icon icon-caret-down right-4 top-1/2"
+            ></div>
+          </div>
+        </div>
+
+        <div class="flex items-center w-full gap-4">
+          <DatePicker
+            filterSize="lg"
+            :activePeriod="activePeriod"
+            @onFilterSelected="processFilterSelection"
+          />
+          <button
+            @click="exportToExcel"
+            class="export-btn w-48 sm:w-1/2 p-4 border rounded-md font-semibold text-sm text-teal-800 hover:bg-teal-50 transition-all duration-200"
+          >
+            Export
+          </button>
+        </div>
+      </div>
+    </template>
 
     <template #pageContent>
       <TableContainer
@@ -109,9 +109,6 @@ const isLoading = ref(true);
 const selectedMethod = ref("");
 const selectedStatus = ref("");
 const activePeriod = ref<[Date, Date] | null>(null);
-const showDropdown = ref(false);
-
-
 
 const statusOptions = ["Successful", "Pending", "Failed"];
 const paymentMethods = ["Card", "Mobilemoney"];
@@ -129,7 +126,7 @@ const tableBody = ref<any[]>([]);
 const tableBodyRaw = ref<any[]>([]);
 
 const getTransactionDate = (date: string) => {
-  let { w2, m3, d3, y1 } = useDate.formatDate(date).getAll();
+  const { w2, m3, d3, y1 } = useDate.formatDate(date).getAll();
   return `${w2}, ${d3} ${m3}, ${y1}`;
 };
 
@@ -141,15 +138,12 @@ const normalizeDate = (date: Date) => {
 
 const isWithinRange = (date: Date, range: [Date, Date] | null): boolean => {
   if (!range || !range[0] || !range[1]) return true;
-
   const start = normalizeDate(new Date(range[0]));
   const end = new Date(range[1]);
-  end.setHours(23, 59, 59, 999); 
-
+  end.setHours(23, 59, 59, 999);
   const target = new Date(date);
   return target >= start && target <= end;
 };
-
 
 const processFilterSelection = (
   selectedRange: [Date | string, Date | string]
@@ -171,13 +165,13 @@ const fetchPaymentTransactions = async () => {
     payload: {},
     showAlert: false,
   });
+
   isLoading.value = false;
+
   if (response?.code === 200) {
     tableBody.value = response.data.map((data: any) => {
-      const formattedAmount = `${data.currency} ${formatNumber(data.amount)}`;
-      const chargeAmount = `Charge: ${data.currency} ${formatNumber(
-        data.charge
-      )}`;
+      const formattedAmount = `${formatNumber(data.amount)}`;
+      const chargeAmount = `Charge: ${data.currency} ${formatNumber(data.charge)}`;
       const customerName = data.customer
         ? `${data.customer.firstname} ${data.customer.lastname}`
         : "No customer info";
@@ -199,7 +193,7 @@ const fetchPaymentTransactions = async () => {
           date_created: getTransactionDate(data.created_at),
           raw_date: createdDate,
           customer_details: `${customerName} (${customerEmail})`,
-          amount: `${formattedAmount} (${chargeAmount})`,
+          amount: `${formattedAmount}`,
           payment_details: capitalizeFirstLetter(data.method),
           status: data.status,
           reference: data.reference,
@@ -212,12 +206,12 @@ const fetchPaymentTransactions = async () => {
 
 const filteredTableBody = computed(() => {
   return tableBody.value.filter((tx) => {
-    const method = tx.raw?.payment_details;
-    const status = tx.raw?.status;
+    const method = tx.raw?.payment_details?.toLowerCase();
+    const status = tx.raw?.status?.toLowerCase();
     const rawDate = tx.raw?.raw_date ? new Date(tx.raw.raw_date) : null;
 
     const matchesMethod = selectedMethod.value
-      ? method === selectedMethod.value
+      ? method === selectedMethod.value.toLowerCase()
       : true;
 
     const matchesStatus = selectedStatus.value
@@ -227,8 +221,8 @@ const filteredTableBody = computed(() => {
     const matchesDate = rawDate
       ? isWithinRange(rawDate, activePeriod.value)
       : true;
-      
-    return matchesMethod && matchesStatus && matchesDate 
+
+    return matchesMethod && matchesStatus && matchesDate;
   });
 });
 
@@ -245,7 +239,7 @@ const exportToExcel = () => {
   const worksheet = XLSX.utils.json_to_sheet(cleanData);
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, "Merchant Transactions");
-  XLSX.writeFile(workbook, "transactions.xlsx");
+  XLSX.writeFile(workbook, "Merchant_Transactions.xlsx");
 };
 
 onMounted(fetchPaymentTransactions);
@@ -254,15 +248,12 @@ onMounted(fetchPaymentTransactions);
 <style lang="scss" scoped>
 .export-btn {
   @apply transition-colors duration-200;
-
   @media (max-width: 640px) {
     @apply w-1/2;
   }
 }
 
 .filter-select {
-
-
   @media (max-width: 640px) {
     @apply w-full;
   }
